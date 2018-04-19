3b579167f93624022db21d704b5405478ff66f0f
sphoortikulkarni <sphoorti19@gmail.com>
2018-04-19 22:10:38 +0530

    Update __init__.py

--- gym_soccer/__init__.py
+++ gym_soccer/__init__.py
@@ -4,25 +4,9 @@ from gym.envs.registration import register
 logger = logging.getLogger(__name__)
 
 register(
-    id='Soccer-v0',
-    entry_point='gym_soccer.envs:SoccerEnv',
+    id='Car-v0',
+    entry_point='car_race.envs:CarEnv',
     timestep_limit=1000,
     reward_threshold=1.0,
-    nondeterministic = True,
-)
-
-register(
-    id='SoccerEmptyGoal-v0',
-    entry_point='gym_soccer.envs:SoccerEmptyGoalEnv',
-    timestep_limit=1000,
-    reward_threshold=10.0,
-    nondeterministic = True,
-)
-
-register(
-    id='SoccerAgainstKeeper-v0',
-    entry_point='gym.envs:SoccerAgainstKeeperEnv',
-    timestep_limit=1000,
-    reward_threshold=8.0,
     nondeterministic = True,
 )
